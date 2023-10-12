c43bbb544f8bb03d83f3da664e9c01c50b854748
Ruslan Ismailov <rusishss@mail.ru>
2023-10-13 00:41:59 +0600

    typeOrm to neon

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -8,33 +8,33 @@ const port = process.env.PGPORT
 //const dataBaseUrl = process.env.SQL_URPGUSERL
 const hostName = process.env.PGHOST
 
-// const typeOrmConfig: TypeOrmModuleOptions = {
-//   type: 'postgres',
-//   host: 'localhost',
-//   port: 5432,
-//   username: 'nest',
-//   password: 'nest',
-//   database: 'blogs_and_posts',
-//   synchronize: false,
-// };
-
 const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: hostName,
-  port: +port,
-  username: userName,
-  password: password,
-  database: dataBaseName,
-  autoLoadEntities: false,
-  //options: { encrypt: false },
-  logging: false,
-  synchronize: true,
-  ssl: true,
-  extra: {
-      ssl: {
-          rejectUnauthorized: false,
-      },
-  },
+  host: 'localhost',
+  port: 5432,
+  username: 'nest',
+  password: 'nest',
+  database: 'blogs_and_posts',
+  synchronize: false,
 };
+
+// const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: hostName,
+//   port: +port,
+//   username: userName,
+//   password: password,
+//   database: dataBaseName,
+//   autoLoadEntities: false,
+//   //options: { encrypt: false },
+//   logging: false,
+//   synchronize: true,
+//   ssl: true,
+//   extra: {
+//       ssl: {
+//           rejectUnauthorized: false,
+//       },
+//   },
+// };
 
 export default typeOrmConfig;
